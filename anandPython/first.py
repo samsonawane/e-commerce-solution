2a367d69cedcbb19c0a996e8b70bb9f22b46706e
anand0122 <ananddubey0122@gmail.com>
2022-05-03 14:51:39 +0530

    Add two numbers

---
+++ anandPython/first.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the 1st number : "))
+num2 = int(input("Enter the 2nd number : "))
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
