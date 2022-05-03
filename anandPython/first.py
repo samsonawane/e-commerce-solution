8a6fed4a167db92c0ff07cbedc9361b7e0dc8227
--global <ananddubey0122@gmail.com>
2022-05-03 15:05:48 +0530

    Change in comment

--- anandPython/first.py
+++ anandPython/first.py
@@ -4,5 +4,5 @@ num2 = int(input("Enter the 2nd number : "))
 # Add two numbers
 sum = num1 + num2
 
-# Display the sum
+# Display the sum of both the number
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
